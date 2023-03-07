48edefd0c32447e47498cb67ac7bf48bc1c640c5
Cabadga <121972248+Cabadga@users.noreply.github.com>
2023-03-07 14:32:20 -0800

    Challenge 4 Bonus 1 Finished

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -6,6 +6,7 @@ public class PlayerControllerX : MonoBehaviour
 {
     private Rigidbody playerRb;
     private float speed = 500;
+    public float boost = 10;
     private GameObject focalPoint;
 
     public bool hasPowerup;
@@ -29,7 +30,11 @@ public class PlayerControllerX : MonoBehaviour
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(focalPoint.transform.forward * boost,ForceMode.Impulse);
 
+        }
     }
 
     // If Player collides with powerup, activate powerup
@@ -40,6 +45,7 @@ public class PlayerControllerX : MonoBehaviour
             Destroy(other.gameObject);
             hasPowerup = true;
             powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
         }
     }
 
@@ -57,7 +63,7 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
+            Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position); 
            
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
